6dd853ce33259591b381a544551e0e88f1aa5994
adityagarud <garudaditya079@gmail.com>
2025-09-29 22:49:52 +0530

    fix(i18n): update default locale handling in getUserLocale function

--- src/lib/i18n-helper.ts
+++ src/lib/i18n-helper.ts
@@ -1,15 +1,14 @@
 'use server';
 
-import { Locale } from "@/i18n/config";
-import { defaultLocale } from "@/i18n/config";
 import { cookies } from "next/headers";
 
 const COOKIE_NAME = "NEXT_LOCALE";
+const DEFAULT_LOCALE = "en";
 
 export async function getUserLocale() {
-  return (await cookies()).get(COOKIE_NAME)?.value || defaultLocale;
+  return (await cookies()).get(COOKIE_NAME)?.value || DEFAULT_LOCALE;
 }
 
-export async function setUserLocale(locale: Locale) {
+export async function setUserLocale(locale: string) {
   (await cookies()).set(COOKIE_NAME, locale);
 }
