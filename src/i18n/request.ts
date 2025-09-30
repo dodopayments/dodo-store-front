e2e60e7418df9503cac65283c1af16051861f798
adityagarud <garudaditya079@gmail.com>
2025-09-30 21:13:31 +0530

    chore: finish lingo.dev integration and demo (locale UX, cleanup)

--- src/i18n/request.ts
+++
@@ -1,3 +0,0 @@
-// next-intl integration removed in favor of lingo.dev Compiler
-// This file remains as a noop module to avoid import errors if referenced.
-export {};
--- src/lib/i18n-helper.ts
+++ src/lib/i18n-helper.ts
@@ -8,7 +8,16 @@ import { cookies } from "next/headers";
 const COOKIE_NAME = "lingo-locale";
 
 export async function getUserLocale() {
-  return (await cookies()).get(COOKIE_NAME)?.value || defaultLocale;
+  const cookieStore = await cookies();
+  let locale = cookieStore.get(COOKIE_NAME)?.value;
+  if (!locale) {
+    const oldLocale = cookieStore.get("NEXT_LOCALE")?.value;
+    if (oldLocale) {
+      cookieStore.set(COOKIE_NAME, oldLocale);
+      locale = oldLocale;
+    }
+  }
+  return locale || defaultLocale;
 }
 
 export async function setUserLocale(locale: Locale) {
